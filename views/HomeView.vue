c6b6c920d7f53a717e39b427e2d32948a67b5bbe
goohayami <goohayami@gmail.com>
2022-12-18 13:39:36 +0900

    pikachu追加

--- views/HomeView.vue
+++ views/HomeView.vue
@@ -5,19 +5,11 @@
       ポケットモンスターは、株式会社ポケモンから発売されているゲームソフトシリーズの名称。また、同作品に登場する架空の生物の総称、それらを題材にしたアニメを始めとするメディアミックス作品群を指す。略称は「ポケモン」。
       ウィキペディア
     </p>
+    <img src="assets/pikachu.png" style="width: 50%" alt="" />
   </section>
-  <img01Path />
 </template>
 
-<script>
-export default {
-  data() {
-    return {
-      img01Path: "../assets/pikachu.png",
-    };
-  },
-};
-</script>
+<script></script>
 
 <style>
 /* ここにｃｓｓ */
