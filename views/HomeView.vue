6a764621a0406946ffe486fa6873949e7e029eb3
goohayami <goohayami@gmail.com>
2022-12-18 13:42:53 +0900

    ぴか

--- views/HomeView.vue
+++ views/HomeView.vue
@@ -5,8 +5,9 @@
       ポケットモンスターは、株式会社ポケモンから発売されているゲームソフトシリーズの名称。また、同作品に登場する架空の生物の総称、それらを題材にしたアニメを始めとするメディアミックス作品群を指す。略称は「ポケモン」。
       ウィキペディア
     </p>
-    <img src="assets/pikachu.png" style="width: 50%" alt="" />
   </section>
+  <img style="width: 35%" src="assets/pikachu.png" alt="" />
+  <br><br><br>
 </template>
 
 <script></script>
