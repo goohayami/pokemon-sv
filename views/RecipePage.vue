e51b7070825e454d472d92fca4adbc0917ec9afd
goohayami <goohayami@gmail.com>
2022-12-28 14:21:23 +0900

    resipe

---
+++ views/RecipePage.vue
@@ -0,0 +1,17 @@
+<template>
+  <section>
+    <h3>時短レシピ</h3>
+    <br />
+    <img style="width: 103%" src="assets/spa1.png" alt="" />
+    <img style="width: 103%" src="assets/spa2.png" alt="" />
+    <img style="width: 103%" src="assets/spa3.png" alt="" />
+    <img style="width: 103%" src="assets/spa4.png" alt="" />
+
+    <br /><br /><br /><br />
+  </section>
+</template>
+<style>
+h3 {
+  margin-bottom: 0;
+}
+</style>
